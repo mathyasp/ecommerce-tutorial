5cf796d7d9eabc38c756d8075c3b28a1044e6d12
m p <mtpap@MacBook-Pro.local>
2023-09-22 14:57:18 -0400

    added removeItem function

--- scripts.js
+++ scripts.js
@@ -97,3 +97,18 @@ function getTotal() {
 	};
 	return total.toFixed(2);
 };
+
+// Function to remove item from the shopping cart
+function removeItem(name, qty = 0) {
+	for (let i = 0; i < cart.length; i += 1) {
+		if (cart[i].name === name) {
+			if (qty > 0) {
+				cart[i].qty -= qty;
+			};
+			if (cart[i].qty < 1 || qty === 0) {
+				cart.splice(i, 1);
+			};
+			return;
+		};
+	};
+};
