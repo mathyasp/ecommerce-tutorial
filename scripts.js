99045da117288e9dce6d64c8ccbe70213cac761d
m p <mtpap@MacBook-Pro.local>
2023-09-22 14:48:44 -0400

    displayed cart items on console

--- scripts.js
+++ scripts.js
@@ -58,11 +58,42 @@ const cart = [];
 
 // Function to add an item to the shopping cart
 function addItem(name, price) {
-	const item = {name: name, price: price, qty: 1};
+	for (let i = 0; i < cart.length; i += 1) {
+		if (cart[i].name === name) {
+			cart[i].qty += 1;
+			return;
+		}
+	};
+	const item = {name, price, qty: 1};
 	cart.push(item);
 };
 
 // Function to show items in the shopping cart
 function showItems() {
-	console.log(`You have ${cart.length} items in your cart`);
+	const qty = getQty();
+	console.log(`You have ${qty} items in your cart`);
+
+	for (let i = 0; i < cart.length; i += 1) {
+		console.log(`${cart[i].name} ${cart[i].price} x ${cart[i].qty}`);
+	};
+	
+	console.log(`Total in cart: $${getTotal()}`);
+};
+
+// Function to get the quantity of items in the shopping cart
+function getQty() {
+	let qty = 0;
+	for (let i = 0; i < cart.length; i += 1) {
+		qty += cart[i].qty;
+	};
+	return qty;
+};
+
+// Function to get the total price of the shopping cart
+function getTotal() {
+	let total = 0;
+	for (let i = 0; i < cart.length; i += 1) {
+		total += cart[i].price * cart[i].qty;
+	};
+	return total.toFixed(2);
 };
